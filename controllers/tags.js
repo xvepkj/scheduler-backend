6234216fe4bac091115301d3a94a54aad38c7a0a
terxor <39589847+terxor@users.noreply.github.com>
2022-11-10 09:45:30 +0530

    Complete tags flow and other minor changes (#22)
    * Complete tags flow and other minor changes
    
    * Lint fix

--- controllers/tags.js
+++ controllers/tags.js
@@ -13,11 +13,47 @@ var tagsController = {};
 tagsController.add = (req, res) => {
   const tag = req.body;
   if (!tag.color || !tag.name) {
-    res.json({ errorMessage: ec.tags.INVALID_REQ});
+    res.json({ errorMessage: ec.tags.INVALID_SCHEMA});
   } else {
-    tag.id = data.counter++;
+    tag.id = (data.counter++).toString();
     data.tags.push(tag);
     res.json(tag);
+  }
+};
+
+tagsController.update = (req, res) => {
+  const tag = req.body;
+  if (!tag.color || !tag.name) { // replace by schema validation
+    res.json({ errorMessage: ec.tags.INVALID_SCHEMA});
+    return;
+  }
+  var index = data.tags.findIndex(t => t.id === tag.id);
+  if (index === -1) {
+    res.json({ errorMessage: ec.tags.INVALID_ID });
+  } else {
+    data.tags[index] = tag;
+    res.json(tag);
+  }
+};
+
+tagsController.delete = (req, res) => {
+  var id = req.body.id;
+  var index = data.tags.findIndex(t => t.id === id);
+  if (index === -1) {
+    res.json({ errorMessage: ec.tags.INVALID_ID });
+  } else {
+    data.tags.splice(index, 1);
+    res.json({ message: "Success" });
+  }
+};
+
+tagsController.getById = (req, res) => {
+  var id = req.params.id;
+  var index = data.tags.findIndex(t => t.id === id);
+  if (index === -1) {
+    res.json({ errorMessage: ec.tags.INVALID_ID });
+  } else {
+    res.json(data.tags[index]);
   }
 };
 
